b1655e83c809089e8841a58bd53c3b13b258f82a
Saran <sarang@covalsys.com>
2025-08-07 12:14:19 +0530

    Initial commit

--- src/src/pages/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Typography } from '@mui/material';
-
-const NotFound = () => (
-  <Typography variant="h4" textAlign={'center'}>404 | Page Not Found</Typography>
-);
-
-export default NotFound;
